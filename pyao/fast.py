17c249475f0d7829393269fd76a15a470d33ab56
worldmozara <worldmozara@gmail.com>
2022-04-29 12:28:44 +0800

    introduce fast_play_sine to fast.py.

--- pyao/fast.py
+++ pyao/fast.py
@@ -2,6 +2,13 @@
 A set of small tools to make some simple sounds conveniently.
 """
 
+__all__ = [
+    'FastPlay',
+    'playbytes',
+    'play2file',
+    'fast_play_sine',
+]
+
 from math import pi, sin
 
 from pyao._abstract import (
@@ -9,8 +16,17 @@ from pyao._abstract import (
     AO
 )
 
+from pyao._aointernal import pyao_fast_play_sine
+
 playbytes = AO.fast_play
 play2file = AO.fast_play_file
+
+
+def fast_play_sine(driver: int, format: AOFormat, freq: float = 440.0, duration: float = 1.0, volume: float = 1.0):
+    """
+    Play a sine wave with the given frequency, duration and volume.
+    """
+    pyao_fast_play_sine(driver, format.bits, format.channels, format.rate, format.byte_format, format.mat, freq, duration, volume)
 
 
 class FastPlay:
