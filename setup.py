54c6cee30a95eb419cba4d7018163e1859003013
worldmozara <worldmozara@gmail.com>
2022-05-03 10:25:36 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup, Extension
+from os import path, getenv
+
+ext = Extension(
+    'pyao._aointernal',
+    sources=['pyao/_aointernal.c'],
+    include_dirs=[
+        path.join(getenv('PREFIX', '/usr'), 'include', 'ao'),
+    ],
+    libraries=['ao', 'm'],
+    library_dirs=[path.join(getenv('PREFIX', '/usr'), 'lib', 'ao')],
+)
+
+setup(
+    name='pyao',
+    version='0.1.0',
+    description='Python libao interface',
+    author='NCBM',
+    author_email='worldmozara@gmail.com',
+    url='https://github.com/NCBM/pyao',
+    packages=['pyao'],
+    ext_modules=[ext],
+)
