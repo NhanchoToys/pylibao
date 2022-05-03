a8005bb730524b1e9ed5d62f39dbc3d66b1121d0
worldmozara <worldmozara@gmail.com>
2022-05-03 11:35:18 +0800

    adjust setup.

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
-from distutils.core import setup, Extension
-from os import path, getenv
+from os import getenv, path
+from setuptools import setup, Extension
 
 ext = Extension(
     'pyao._aointernal',
     sources=['pyao/_aointernal.c'],
-    include_dirs=[
-        path.join(getenv('PREFIX', '/usr'), 'include', 'ao'),
-    ],
     libraries=['ao', 'm'],
     library_dirs=[path.join(getenv('PREFIX', '/usr'), 'lib', 'ao')],
+    include_dirs=[path.join(getenv('PREFIX', '/usr'), 'include', 'ao')],
 )
 
+
 setup(
-    name='pyao',
-    version='0.1.0',
-    description='Python libao interface',
+    name='pylibao',
+    version='0.1.1',
+    description='Python library for accessing the Libao API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='HivertMoZara',
     author_email='worldmozara@gmail.com',
     url='https://github.com/NCBM/pyao',
     packages=['pyao'],
     ext_modules=[ext],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='audio',
+    license='MIT',
 )
