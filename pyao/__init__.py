3d7f7cf62a90c73b0d1f157e912fb4389890fb38
worldmozara <worldmozara@gmail.com>
2022-08-30 09:41:51 +0800

    fix error.

--- pyao/__init__.py
+++ pyao/__init__.py
@@ -39,41 +39,40 @@ init()
 atexit.register(shutdown)
 
 
+def get_format_from_string(s: str, matrix: str = "L,R") -> ao_sample_format:
+    """
+    Get ao_sample_format from string.
+
+    :param s: format string
+    :param matrix: channel matrix
+
+    :return: ao_sample_format
+    """
+    exp = re.compile(
+        r"^[Bb](?P<bits>\d+)[Cc](?P<channels>\d+)"
+        r"[Rr](?P<rate>\d+)(?P<byte_format>[LB])E?$"
+    )
+    m = exp.match(s)
+
+    def err_invalid_format():
+        raise ValueError(f"Invalid format string: {s}")
+
+    if m is None:
+        err_invalid_format()
+
+    return ao_sample_format(
+        int(m.group("bits")),
+        int(m.group("channels")),
+        int(m.group("rate")),
+        AO_FMT_LITTLE if m.group("byte_format") == "L" else AO_FMT_BIG
+        if m.group("byte_format") == "B" else err_invalid_format(),
+        matrix
+    )
+
+
 class _preset:
-    def get_format_from_string(
-        self, s: str, matrix: str = "L,R"
-    ) -> ao_sample_format:
-        """
-        Get ao_sample_format from string.
-
-        :param s: format string
-        :param matrix: channel matrix
-
-        :return: ao_sample_format
-        """
-        exp = re.compile(
-            r"^[Bb](?P<bits>\d+)[Cc](?P<channels>\d+)"
-            r"[Rr](?P<rate>\d+)(?P<byte_format>[LB])E?$"
-        )
-        m = exp.match(s)
-
-        def err_invalid_format():
-            raise ValueError(f"Invalid format string: {s}")
-
-        if m is None:
-            err_invalid_format()
-
-        return ao_sample_format(
-            int(m.group("bits")),
-            int(m.group("channels")),
-            int(m.group("rate")),
-            AO_FMT_LITTLE if m.group("byte_format") == "L" else AO_FMT_BIG
-            if m.group("byte_format") == "B" else err_invalid_format(),
-            matrix
-        )
-
     def __getattribute__(self, __name: str) -> ao_sample_format:
-        return self.get_format_from_string(__name)
+        return get_format_from_string(__name)
 
 
 preset = _preset()
